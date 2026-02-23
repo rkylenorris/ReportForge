ab800f41eec83e7ea1b4e0ab8b75b126d5cbe585
Kyle Norris <r.kyle.norris@gmail.com>
2026-02-23 13:01:14 -0500

    continued work on Run Manifest classes, specifically a method for getting the previous schema hash to compare with current.

--- src/dotnet/ReportForge.Logging/ReportForge.Logging.Metadata/RunManifest.cs
+++ src/dotnet/ReportForge.Logging/ReportForge.Logging.Metadata/RunManifest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ReportForge.Logging.Metadata;
 
@@ -99,15 +100,42 @@ public class DatasetMetric
 
     // A hash of column names+types. 
     // Changes if SQL schema changes, alerting you to breaking changes.
-    // TODO: Decide method for comparing current hash to previous or expected.
-    public string SchemaHash { get; set; }
+    public string PreviousSchemaHash { get; private set; }
+    public string SchemaHash { get; private set; }
 
-    public DatasetMetric(string datasetName, long rowCount, double queryDurationMs, string output, string schemaHash)
+    private string FileFriendlyName;
+
+    public DatasetMetric(string datasetName, long rowCount, double queryDurationMs, string outputDir, string schemaHash)
     {
         Name = datasetName;
+        FileFriendlyName = Regex.Replace(Name.Trim(), @"[^a-zA-Z0-9.\s_-]", "");
         RowCount = rowCount;
         DurationMs = queryDurationMs;
-        ParquetPath = output;
+        ParquetPath = Path.Join(outputDir, string.Format("{0}.parquet", FileFriendlyName));
         SchemaHash = schemaHash;
+    }
+
+    private string GetPreviousSchemaHash(string outputDir)
+    {
+        string hash = string.Empty;
+        string hashPath = Path.Join(outputDir, "schema_hash.txt");
+        DateTime lastHashWriteUtc = File.GetLastWriteTimeUtc(hashPath);
+        DateTime lastHashWriteLocal = lastHashWriteUtc.ToLocalTime();
+        hash = File.ReadAllText(hashPath, Encoding.UTF8).Trim('\n').Trim();
+
+        string archivesPath = Path.Join(Environment.CurrentDirectory, "archives");
+
+        if (!Path.Exists(archivesPath))
+        {
+            File.Create(archivesPath);
+        }
+
+        string archiveHashPath = Path.Join(archivesPath, string.Format("schema_hash_{0}.txt", lastHashWriteLocal.ToString("yyyy-MM-dd_HH-mm")));
+
+        File.WriteAllText(archiveHashPath, hash);
+
+        File.Delete(hashPath);
+
+        return hash;
     }
 }
